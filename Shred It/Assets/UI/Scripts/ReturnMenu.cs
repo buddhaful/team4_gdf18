fdbedb7ab15f82f8404bd0a8b4a88ee8f34a6cd7
Virginia Lopez-Nadal <vcl16@my.fsu.edu>
2018-11-29 11:15:54 -0500

    Need skier graphic (might adjust it more w/ shading, etc.), Created a scene manager (buttons on start up now work), Volume adjusted to stay for each scene

---
+++ Shred It/Assets/UI/Scripts/ReturnMenu.cs
@@ -0,0 +1,10 @@
+﻿ using UnityEngine;
+ using System.Collections;
+ using UnityEngine.SceneManagement;
+ 
+ public class ReturnMenu : MonoBehaviour 
+ {
+     public void SceneSwitcher() {
+         SceneManager.LoadScene (0);
+     }
+ }
